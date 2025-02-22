9d727108c055e231a1fe9a00b795fb28270f48e1
Bohdan Danylko <malyyboh@gmail.com>
2025-02-22 11:18:14 +0100

    hw06 Bohdan Danylko

--- lessons/lesson01/hw01.py
+++ lessons/lesson01/hw01.py
@@ -5,7 +5,7 @@
 and will read the user's responses and display the corresponding messages:
       “Hello, (answer(name))“.
       “Your age is  (answer(age))“.
-      “You live in  (answer(city))“.   """
+      “You live in  (answer(city))“."""
 
 # HW01
 name = input("What is your name? ")
@@ -14,5 +14,3 @@ city = input("Where do you live? ")
 print("Hello, " + name)
 print("Your age is " + age)
 print("You live in " + city)
-
-
