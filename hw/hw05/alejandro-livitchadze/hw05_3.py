db2ea79a13471e8c2e87678f13ebb2c962ef7eca
Oleksandr Livitchuk <saddy802@gmail.com>
2025-02-23 18:12:00 +0200

    Practical Task 1 [Lesson 5]

---
+++ hw/hw05/alejandro-livitchadze/hw05_3.py
@@ -0,0 +1,10 @@
+def factorial(num):
+    if num == 0:
+        print(1)
+    else:
+        result = 1
+        for i in range(1, num + 1):
+            result *= i
+        print(result)
+
+factorial(3)
