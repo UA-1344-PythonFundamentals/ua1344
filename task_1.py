d4d1913e54a506cd058cc23977df7276c2816c62
r.kryuk <retsediv@gmail.com>
2025-03-16 22:34:23 +0100

    hm06

--- task_1.py
+++
@@ -1,8 +0,0 @@
-# Створюємо список цілих чисел
-int_list = [3, 5, 7, 9, 11]
-
-# Конвертуємо кожен елемент у float за допомогою циклу
-float_list = [float(num) for num in int_list]
-
-# Виводимо результат
-print(float_list)
