8d812344c678c5e65794448504c724d111f279d0
Olena Moroz <olena.s.moroz@gmail@com>
2025-03-30 19:03:28 +0300

    added hw05

---
+++ hw/hw05/OlenaMoroz/Task 1.py
@@ -0,0 +1,12 @@
+#Use the loop to change integer type to a floating type
+int_list = [1,2,3,4,5,6,7,8,9,10]
+float_list = []
+for num in int_list:
+ float_list.append(float(num))
+print("Original List:", (int_list))
+print("Changed List:", (float_list))
+
+
+
+  
+  
