2c0b3773fa3331f1ec6cb4a7f2b86fac8eb5810e
Pasha Kisera <pasha.kisera@gmail.com>
2025-04-17 17:46:19 +0300

    hw10

---
+++ hw/hw10/pashakisera/1.py
@@ -0,0 +1,21 @@
+# Task 1
+# Create a polygon class and a rectangle class that inherits from the polygon class and finds the square of rectangle.
+
+class Polygon:
+    def __init__(self, pol_side):
+        self.pol_side = pol_side
+
+
+class Rectangle(Polygon):
+
+    def __init__(self, a, b):
+        super().__init__(4)
+        self.a = a
+        self.b = b
+
+    def square(self):
+        return self.a * self.b
+
+
+rect = Rectangle(5, 10)
+print(rect.square())
---
+++ hw/hw10/pashakisera/2.py
@@ -0,0 +1,26 @@
+# Task 2
+# Create a class Human, everyone has a name, create a method in the class that displays a welcome
+# message to each person. Create a class method in the class that returns information that
+# it is a species of "Homosapiens". And in the class create a static method that returns an arbitrary message.
+
+class Human:
+    def __init__(self, name):
+        self.name = name
+
+    def welc_mesg(self):
+        return "Welcome " + self.name
+
+    @classmethod
+    def info_mesg(cls):
+        return "You are  Homosapiens"
+
+    # a static method that returns a message
+    @staticmethod
+    def mesg():
+        return "Hello world"
+
+
+person = Human("Jack")
+print(person.welc_mesg())
+print(person.info_mesg())
+print(person.mesg())
